6df18d266847a97508881b8d69853a218f9f83e8
TheSharkFullCode <agedjoscar@hotmail.com>
2024-05-04 19:37:42 +0200

    donde

---
+++ vite-vue-intro/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+
+  <h1 class="flex justify-center text-5xl">{{ msg }}</h1>
+
+  <div class="card">
+    
+    <button type="button" @click="count++" class="border-[3px] rounded border-[pink] bg-[#53dcb5] p-5">count is {{ count }}</button>
+
+  </div>
+
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
